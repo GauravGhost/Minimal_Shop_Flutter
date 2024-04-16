c80c0d6fbc9a6529462a7a67e4bbc5e7a9bdb3ed
Gyanendra Kumar <gyanendrak874@gmail.com>
2024-04-16 15:22:05 +0530

    Added shop models

--- lib/models/shop.dart
+++ lib/models/shop.dart
@@ -1,15 +1,48 @@
-class Shop {
+import 'dart:js_interop';
 
+import 'package:minimal_shop/models/product.dart';
+
+class Shop {
   // List of product for sale
+  final List<Product> _shop = const [
+    Product(
+      name: "Product 1",
+      description: "This is description of product 1",
+      imagePath: "lib/assets/product1.png",
+      price: 22.22,
+    ),
+    Product(
+      name: "Product 2",
+      description: "This is description of product 2",
+      imagePath: "lib/assets/product2.png",
+      price: 22.22,
+    ),
+    Product(
+      name: "Product 3",
+      description: "This is description of product 3",
+      imagePath: "lib/assets/product3.png",
+      price: 22.22,
+    ),
+    Product(
+      name: "Product 4",
+      description: "This is description of product 4",
+      imagePath: "lib/assets/product4.png",
+      price: 22.22,
+    )
+  ];
 
   // user cart
+  List<Product> _userCart = [];
 
   // get product list
+  List<Product> get shop => _shop;
 
   // get user cart
+  List<Product> get cart => _userCart;
 
-  // add item to cart 
+  // add item to cart
+  void addToCart(Product item) => _userCart.add(item);
 
   // remove item from cart
-  
+  void removeFromCart(Product item) => _userCart.remove(item);
 }
